17c221ef8b75d800aa8c71f21a4dbd9cc11f05ce
Wilkinson <wilkinson.wkn@gmail.com>
2019-09-02 12:25:27 -0300

    fatiando o layout

---
+++ footer.php
@@ -0,0 +1,119 @@
+<footer class="footer-area">
+        <div class="container">
+            <div class="row justify-content-between">
+                <div class="col-sm-6 col-xl-3">
+                    <div class="single-footer-widget footer_1">
+                        <a href="index.html"> <img src="<?php bloginfo('template_url')?>/img/footer-logo.png" alt=""> </a>
+                        <p>Created. Image moving living fowl earth fruitful. Two hath first you're doesn you
+                            life above. Living give and earth light for appear moved their behold </p>
+                        <div class="social_icon">
+                            <a href="#"> <i class="ti-facebook"></i> </a>
+                            <a href="#"> <i class="ti-twitter-alt"></i> </a>
+                            <a href="#"> <i class="ti-instagram"></i> </a>
+                            <a href="#"> <i class="ti-skype"></i> </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-xl-3">
+                    <div class="single-footer-widget">
+                        <h4>Nossos Serviços</h4>
+                        <ul>
+                            <li><a href="#">Car accident</a></li>
+                            <li><a href="#">Personal injury</a></li>
+                            <li><a href="#">Family law</a></li>
+                            <li><a href="#">Bank and financial</a></li>
+                            <li><a href="#">Capital market</a></li>
+                            <li><a href="#">Employment Law</a></li>
+                        </ul>
+
+                    </div>
+                </div>
+                <div class="col-sm-6 col-xl-3">
+                    <div class="single-footer-widget footer_icon">
+                        <h4>Informações de Contato</h4>
+                        <p>Q. 104 Norte Rua NE 5, 38 - Plano Diretor Norte, Palmas, TO</p>
+                        <ul>
+                            <li><a href="#"><i class="ti-mobile"></i>(63) 3215-5293</a></li>
+                            <li><a href="mailto:ariclaw@law.com"><i class="ti-email"></i>ariclaw@law.com</a></li>
+                            <li><a href="#"><i class="ti-world"></i> ariclawyerfirm.com</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-xl-3">
+                    <div class="single-footer-widget footer_3">
+                        <h4>Instagram</h4>
+                        <div class="footer_img">
+                            <div class="single_footer_img">
+                                <img src="<?php bloginfo('template_url')?>/img/footer_img/footer_img_1.png" alt="">
+                                <a href="#"><i class="ti-instagram"></i></a>
+                            </div>
+                            <div class="single_footer_img">
+                                <img src="<?php bloginfo('template_url')?>/img/footer_img/footer_img_2.png" alt="">
+                                <a href="#"><i class="ti-instagram"></i></a>
+                            </div>
+                            <div class="single_footer_img">
+                                <img src="<?php bloginfo('template_url')?>/footer_img/footer_img_3.png" alt="">
+                                <a href="#"><i class="ti-instagram"></i></a>
+                            </div>
+                            <div class="single_footer_img">
+                                <img src="<?php bloginfo('template_url')?>/img/footer_img/footer_img_4.png" alt="">
+                                <a href="#"><i class="ti-instagram"></i></a>
+                            </div>
+                            <div class="single_footer_img">
+                                <img src="<?php bloginfo('template_url')?>/img/footer_img/footer_img_5.png" alt="">
+                                <a href="#"><i class="ti-instagram"></i></a>
+                            </div>
+                            <div class="single_footer_img">
+                                <img src="<?php bloginfo('template_url')?>/img/footer_img/footer_img_6.png" alt="">
+                                <a href="#"><i class="ti-instagram"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="copyright_part_text text-center">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-lg-12 col-md-12">
+                                    <p class="footer-text m-0 text-center"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<?php echo date('Y');?></script> Todos direitos  reservados
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+                                </div>
+                             
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- footer part end-->
+
+    <!-- jquery plugins here-->
+    <script src="<?php bloginfo('template_url')?>/js/jquery-1.12.1.min.js"></script>
+    <!-- popper js -->
+    <script src="<?php bloginfo('template_url')?>/js/popper.min.js"></script>
+    <!-- bootstrap js -->
+    <script src="<?php bloginfo('template_url')?>/js/bootstrap.min.js"></script>
+    <!-- easing js -->
+    <script src="<?php bloginfo('template_url')?>/js/jquery.magnific-popup.js"></script>
+    <!-- swiper js -->
+    <script src="<?php bloginfo('template_url')?>/js/swiper.min.js"></script>
+    <!-- swiper js -->
+    <script src="<?php bloginfo('template_url')?>/js/masonry.pkgd.js"></script>
+    <!-- particles js -->
+    <script src="<?php bloginfo('template_url')?>/js/owl.carousel.min.js"></script>
+    <!-- swiper js -->
+    <script src="<?php bloginfo('template_url')?>/js/slick.min.js"></script>
+    <script src="<?php bloginfo('template_url')?>/js/gijgo.min.js"></script>
+    <script src="<?php bloginfo('template_url')?>/js/jquery.nice-select.min.js"></script>
+    <!-- custom js -->
+    <script src="<?php bloginfo('template_url')?>/js/custom.js"></script>
+</body>
+
+</html>
